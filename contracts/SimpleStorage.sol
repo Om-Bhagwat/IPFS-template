025a630322092c08391b9b99024a5039a9e5c4ba
om <ombhagwat141@gmail.com>
2021-05-04 04:20:52 +0530

    Connected successfully

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
-  uint storedData;
+  string ipfsHash;
 
-  function set(uint x) public {
-    storedData = x;
+  function set(string memory x) public {
+    ipfsHash = x;
   }
 
-  function get() public view returns (uint) {
-    return storedData;
+  function get() public view returns (string memory) {
+    return ipfsHash;
   }
 }
