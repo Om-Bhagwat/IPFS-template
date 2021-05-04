bd49b2995d7474380c193d55a33a060a1ca98bb8
om <ombhagwat141@gmail.com>
2021-05-04 13:59:09 +0530

    Successfully Connected to infura

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,13 @@ import ipfs from './ipfs';
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0, web3: null, accounts: null, contract: null,buffer:null,ipfsHash:'' };
+
+  constructor(props){
+    super(props)
+    this.captureFile = this.captureFile.bind(this); 
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
   componentDidMount = async () => {
     try {
@@ -49,23 +55,53 @@ class App extends Component {
     this.setState({ storageValue: response });
   };
 
+  captureFile(e){
+    e.preventDefault();
+
+    const file = e.target.files[0];
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file);
+    reader.onloadend=()=>{
+      this.setState({buffer:Buffer(reader.result) })
+      console.log('buffer',this.state.buffer)
+    }
+
+    console.log("captured....")
+  }
+
+  onSubmit = async(e)=>{
+    e.preventDefault();
+
+    const { accounts, contract } = this.state;
+
+    ipfs.files.add(this.state.buffer,(err,result)=>{
+      if(err){
+        console.log(err);
+        return;
+      }else{
+        await contract.methods.set("5").send({ from: accounts[0] });
+        this.setState({ipfsHash:result[0].hash})
+        console.log('ipfsHash',this.state.ipfsHash);
+      }
+    })
+
+    return console.log("Submitted..")
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Your Image</h1>
+        <p>This image is stored on IPFS and Etherum Blockchain</p>
+        <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt="img" />
+        <h2>Upload Image</h2>
+        <form onSubmit={this.onSubmit}>
+            <input type="file" onChange={this.captureFile} />
+            <input type="submit" value="submit"/>
+        </form>
       </div>
     );
   }
